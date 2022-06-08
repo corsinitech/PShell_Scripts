f1b8c01c649573baca61344ca2fae31d1e746249
Gabe Corsini <gabe@corsini.tech>
2022-06-08 18:48:12 -0400

    updated scripts to remove personal info

--- Azure_Authentication.ps1
+++ Azure_Authentication.ps1
@@ -1,15 +1,15 @@
 # USNavyCloud42 Login
-Connect-ExchangeOnline -UserPrincipalName gabriel.corsini.admin@usnavycloud42.onmicrosoft.us -ConnectionUri https://outlook.office365.us/powershell-liveid -AzureADAuthorizationEndPointUri https://login.microsoftonline.us/common
+Connect-ExchangeOnline -UserPrincipalName [enter admin upn here] -ConnectionUri https://outlook.office365.us/powershell-liveid -AzureADAuthorizationEndPointUri https://login.microsoftonline.us/common
 
 #Flankspeed Login
-Connect-ExchangeOnline -UserPrincipalName gabriel.corsini.admin@flankspeed.onmicrosoft.us -ConnectionUri https://webmail.apps.mil/powershell-liveid -AzureADAuthorizationEndPointUri https://login.microsoftonline.us/common
+Connect-ExchangeOnline -UserPrincipalName [enter admin upn here] -ConnectionUri https://webmail.apps.mil/powershell-liveid -AzureADAuthorizationEndPointUri https://login.microsoftonline.us/common
 
 # Cloud42 IPPSSession Login for Security Things
-Connect-IPPSSession -UserPrincipalName gabriel.corsini.admin@usnavycloud42.onmicrosoft.us -ConnectionUri https://l5.ps.compliance.protection.office365.us/powershell-liveid/ -AzureADAuthorizationEndPointUri https://login.microsoftonline.us/common 
+Connect-IPPSSession -UserPrincipalName [enter admin upn here] -ConnectionUri https://l5.ps.compliance.protection.office365.us/powershell-liveid/ -AzureADAuthorizationEndPointUri https://login.microsoftonline.us/common 
 
 # Flankspeed IPPSSession login
-Connect-IPPSSession -UserPrincipalName gabriel.corsini.admin@flankspeed.onmicrosoft.us -ConnectionUri https://webmail.apps.mil/powershell-liveid -AzureADAuthorizationEndPointUri https://login.microsoftonline.us/common 
+Connect-IPPSSession -UserPrincipalName [enter admin upn here] -ConnectionUri https://webmail.apps.mil/powershell-liveid -AzureADAuthorizationEndPointUri https://login.microsoftonline.us/common 
 
-Connect-EXOPSSession -UserPrincipalName gabriel.corsini.admin@flankspeed.onmicrosoft.us -WarningAction SilentlyContinue -AzureADAuthorizationEndpointUri https://login.microsoftonline.us/common -ConnectionUri https://webmail.apps.mil/powershell-liveid/
+Connect-EXOPSSession -UserPrincipalName [enter admin upn here] -WarningAction SilentlyContinue -AzureADAuthorizationEndpointUri https://login.microsoftonline.us/common -ConnectionUri https://webmail.apps.mil/powershell-liveid/
 
-Connect-AzAccount -UserPrincipalName gabriel.corsini.admin@flankspeed.onmicrosoft.us -ConnectionUri https://webmail.apps.mil/powershell-liveid -AzureADAuthorizationEndPointUri https://login.microsoftonline.us/common
+Connect-AzAccount -UserPrincipalName [enter admin upn here] -ConnectionUri https://webmail.apps.mil/powershell-liveid -AzureADAuthorizationEndPointUri https://login.microsoftonline.us/common
